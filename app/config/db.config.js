7f6674bf6558f15fd2ef98485ea30db722c19be3
coderpikachu <1011214027@qq.com>
2020-05-17 10:58:49 +0800

    initial commit

---
+++ app/config/db.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+    HOST: "127.0.0.1",
+    USER: "root",
+    PASSWORD: "SWBswb1436001",
+    DB: "test6",
+    dialect: "mysql",
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};
